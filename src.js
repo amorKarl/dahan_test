07a4f10cb49d95f5f15ae26bfbe3d405bf270de1
amorKarl <1064691217@qq.com>
2017-07-18 17:45:32 +0800

    demo

--- src.js
+++ src.js
@@ -1,18 +1,18 @@
 
-// function ajaxs (argument) {
-// 	// body...
-// 	$.ajax({
-// 		url: 'http://dahan-api.heiyou.net/index.php/home/index/search',
-// 		type: 'POST',
-// 		data: {
-// 			"search": ""
-// 		},
-// 		success:function  (argument) {
-// 			// body...
-// 			console.dir(argument);
-// 		}
-// 	});
-// }
+function ajaxs (argument) {
+	// body...
+	$.ajax({
+		url: 'http://dahan-api.heiyou.net/index.php/home/index/search',
+		type: 'POST',
+		data: {
+			"search": ""
+		},
+		success:function  (argument) {
+			// body...
+			console.dir(argument);
+		}
+	});
+}
 
 // function ajaxs (argument) {
 // 	// body...
@@ -26,17 +26,17 @@
 // 	});
 // }
 
-function ajaxs (argument) {
-	// body...
-	$.ajax({
-		url: 'http://dahan-api.heiyou.net/index.php/home/index/hot_search',
-		type: 'POST',
-		success:function  (argument) {
-			// body...
-			console.dir(argument);
-		}
-	});
-}
+// function ajaxs (argument) {
+// 	// body...
+// 	$.ajax({
+// 		url: 'http://dahan-api.heiyou.net/index.php/home/index/hot_search',
+// 		type: 'POST',
+// 		success:function  (argument) {
+// 			// body...
+// 			console.dir(argument);
+// 		}
+// 	});
+// }
 
 
 
